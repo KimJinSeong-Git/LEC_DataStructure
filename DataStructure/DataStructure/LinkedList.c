cc4456b4a2986b3df240e446d30fde5db4b133f1
hasen49 <hasen49@yonsei.ac.kr>
2022-03-23 16:04:58 +0900

    Refactor: "함수화" - 파일 읽기 함수화 진행 - Search 함수화 진행

--- DataStructure/DataStructure/LinkedList.c
+++ DataStructure/DataStructure/LinkedList.c
@@ -31,6 +31,7 @@ typedef struct LinkedList {
 } linkedList;
 
 // ---------- Prototype ----------
+void readFile(linkedList* L, char* fileRoot);
 linkedList* initList();
 int getLength(linkedList* L);
 
@@ -46,49 +47,16 @@ void reverse(linkedList* L);
 // ---------- Main ----------
 int main() {
 	linkedList* student = initList();
-	listData tempData;
 
 	// ---------- 파일 읽어오기 ----------
-	FILE* fp;
 	char *fileRoot = "C:/Users/YONSEI-IT/Desktop/김진성_근로학생/github/LEC_DataStructure/resource/HW1/student.txt";
-	fopen_s(&fp, fileRoot, "r");
-	
-	// 라인 별, 단어 별 구분하기
-	int cntLine = 1;
-	char fileBuffer[30];
-	char* context = NULL;
-
-	while (!feof(fp)) {
-		fgets(fileBuffer, sizeof(fileBuffer), fp);				// 한 라인 buffer로 읽어오기
-		printf("[Input] line %d\n", cntLine++);
-
-		char* word = strtok_s(fileBuffer, " ", &context);	// 공백을 기준으로 Tokenize
-
-		int cntWord = 1;
-		while (word != NULL) {
-			if (cntWord == 1) {
-				printf("\t- 이름: %s\n", word);
-				strcpy_s(tempData.name, sizeof(word), word);
-				cntWord++;
-			}
-			else if (cntWord == 2) {
-				printf("\t- 학번: %s\n", word);
-				tempData.id = atoi(word);
-				cntWord++;
-			}
-			else
-				printf("ERROR = %s\n", word);
-
-			word = strtok_s(NULL, " ", &context);
-		}
-		insertLast(student, tempData);
-	}
-	fclose(fp);
+	readFile(student, fileRoot);
 
 	// ---------- 터미널 입력 ----------
 	int power = 1;
 	while (power) {
 		char terminalBuffer[100];
+		printf("$ ");
 		gets_s(terminalBuffer, sizeof(terminalBuffer));
 
 		char* cmdContext = NULL;
@@ -101,14 +69,8 @@ int main() {
 		// ---------- Search ----------
 		else if (strcmp(command, "search") == 0) {
 			printf("[System] Search\n");
-
 			int nbSearchID = atoi(strtok_s(NULL, " ", &cmdContext));	// 다음 인자 토큰화 진행
-			listNode* searched = search(student, nbSearchID);			// 해당 ID 탐색
-
-			if (searched != NULL)
-				printf("- 학번이 %d인 학생의 이름: %s\n\n", nbSearchID, searched->data.name);
-			else		// 마지막 링크( NULL )에 도달 즉, 조건에 맞는 노드가 없음
-				printf("- 학번이 %d인 학생이 없습니다.\n\n", nbSearchID);
+			search(student, nbSearchID);								// 해당 ID 탐색
 		}
 
 		// ---------- Insert ----------
@@ -139,6 +101,43 @@ int main() {
 }
 
 // ---------- Definition ----------
+void readFile(linkedList* L, char* fileRoot) {
+	FILE* fp;
+	listData tempData;
+
+	fopen_s(&fp, fileRoot, "r");
+
+	// 라인 별, 단어 별 구분하기
+	int cntLine = 1;
+	char fileBuffer[30];
+	char* context = NULL;
+
+	while (!feof(fp)) {
+		fgets(fileBuffer, sizeof(fileBuffer), fp);				// 한 라인 buffer로 읽어오기
+
+		char* word = strtok_s(fileBuffer, " ", &context);		// 공백을 기준으로 Tokenize
+
+		int cntWord = 1;
+		while (word != NULL) {
+			if (cntWord == 1) {
+				strcpy_s(tempData.name, sizeof(word), word);
+				cntWord++;
+			}
+			else if (cntWord == 2) {
+				tempData.id = atoi(word);
+				cntWord++;
+			}
+			else
+				printf("ERROR = %s\n", word);
+
+			word = strtok_s(NULL, " ", &context);
+		}
+		insertLast(L, tempData);
+	}
+	fclose(fp);
+	printf("[ System ] 파일 불러오기 완료.\n\n");
+}
+
 linkedList* initList() {
 	linkedList* L;									// 리스트 헤드 생성
 	L = (linkedList*)malloc(sizeof(linkedList));	// 메모리 할당
@@ -173,7 +172,7 @@ void insertLast(linkedList* L, listData item) {
 
 void printList(linkedList* L) {
 	listNode* p;
-	printf("[ Print List ]\n");
+	printf("[ System ] Print List\n");
 	
 	p = L->head;
 	while (p != NULL) {
@@ -182,17 +181,24 @@ void printList(linkedList* L) {
 		if (p != NULL)
 			printf("\n");
 	}
-	printf(")\n");
+	printf("\n\n============================================\n\n");
 }
 
 listNode* search(linkedList* L, int x) {
-	listNode* temp;
-	temp = L->head;				// 첫 번째 항목의 주소
-	while (temp != NULL) {		// 공백 리스트가 아니면 반복
-		if (temp->data.id == x)
-			return temp;		// 탐색 데이터가 같다면 해당 노드 반환
+	listNode* searched;
+	searched = L->head;				// 첫 번째 항목의 주소
+	while (searched != NULL) {		// 공백 리스트가 아니면 반복
+		if (searched->data.id == x)
+			break;					// 탐색 데이터가 같다면 반복문 종료
 		else
-			temp = temp->link;	// 같지 않다면 다음 노드 탐색
+			searched = searched->link;		// 같지 않다면 다음 노드 탐색
 	}
-	return temp;
+
+	if (searched != NULL)
+		printf("- 학번이 %d인 학생의 이름: %s\n", x, searched->data.name);
+	else		// 마지막 링크( NULL )에 도달 즉, 조건에 맞는 노드가 없음
+		printf("- 학번이 %d인 학생이 없습니다.\n", x);
+	printf("\n============================================\n\n");
+
+	return searched;
 }
